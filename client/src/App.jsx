9138251de60b01ebcbb7762af03afaf6eac0bb23
Manan <mmpatel8107@gmail.com>
2026-08-30 19:31:57 +0530

    Removed unnecessary pages V1.0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,10 @@ import NavbarLayout from "./layouts/NavbarLayout.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Home from "./pages/Home.jsx";
-import Products from "./pages/Products.jsx";
-import ProductDetails from "./pages/ProductDetails.jsx";
-import Estimation from "./pages/Estimation.jsx";
 import Cart from "./pages/Cart.jsx";
 import Checkout from "./pages/Checkout.jsx";
 import Payment from "./pages/Payment.jsx";
 import Orders from "./pages/Orders.jsx";
-import OrderDetails from "./pages/OrderDetails.jsx";
 
 function App() {
     return (
@@ -25,14 +21,10 @@ function App() {
                     <Route path="/register" element={<Register />} />
                     <Route element={<NavbarLayout />}>
                         <Route path="/" element={<Home />} />
-                        <Route path="/products" element={<Products />} />
-                        <Route path="/products/:id" element={<ProductDetails />} />
-                        <Route path="/estimation" element={<Estimation />} />
                         <Route path="/cart" element={<Cart />} />
                         <Route path="/checkout" element={<Checkout />} />
                         <Route path="/payment/:orderId" element={<Payment />} />
                         <Route path="/orders" element={<Orders />} />
-                        <Route path="/orders/:id" element={<OrderDetails />} />
                     </Route>
                 </Routes>
             </AuthProvider>
